1ce9a688e0d6330d937961ac0bfe5ddcb15463a1
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-06-27 05:54:46 +0530

    First commit

---
+++ model/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const usersSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    default: null,
+  },
+  lastname: {
+    type: String,
+    default: null,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", usersSchema);
